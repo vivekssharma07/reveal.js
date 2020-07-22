969239618a142c3249a9291680ba6df05dcf973d
vivekssharma07 <vivekssharma07@gmail.com>
2020-07-22 13:20:49 +0530

    Initial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [{
+      name        : "Reveal Js",
+      script      : "gulp",
+      watch       : true,
+      merge_logs  : true,
+      cwd         : "/var/www/reveal.js/",
+     }]
+  }
